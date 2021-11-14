6be3d5a745523bec72f2afb71483b8a29d979150
Washim Ahmed <washimahmed@Washims-Mac-mini.local>
2021-11-14 09:40:21 +0530

    updated base content

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='django-frontadmin',
-    version='4.0.6',
+    version='4.0.7',
     author='Washim Ahmed',
     author_email='washim.ahmed@gmail.com',
     packages=find_packages(),
